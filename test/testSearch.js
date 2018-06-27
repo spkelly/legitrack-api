f743f62207e9d0c9e087282c03ac1202787002ec
spkelly <spkelly18@gmail.com>
2018-06-27 13:56:24 -0600

    added a test search object

---
+++ test/testSearch.js
@@ -0,0 +1,65 @@
+module.exports = {
+  'results': {
+    '0': {
+      'relevance': 100,
+      'state': 'CO',
+      'bill_number': 'SB079',
+      'bill_id': 1058746,
+      'change_hash': '84e236d2db7ac2a286860a63617342aa',
+      'url': 'https://legiscan.com/CO/bill/SB079/2018',
+      'text_url': 'https://legiscan.com/CO/text/SB079/2018',
+      'research_url': 'https://legiscan.com/CO/research/SB079/2018',
+      'last_action_date': '2018-04-12',
+      'last_action': 'Governor Signed',
+      'title': 'Sake Vinous Liquor Colorado Liquor Code'
+    },
+    '1': {
+      'relevance': 99,
+      'state': 'CO',
+      'bill_number': 'SB188',
+      'bill_id': 1101365,
+      'change_hash': '2f30b5bc4c284c21a3e2401400579861',
+      'url': 'https://legiscan.com/CO/bill/SB188/2018',
+      'text_url': 'https://legiscan.com/CO/text/SB188/2018',
+      'research_url': 'https://legiscan.com/CO/research/SB188/2018',
+      'last_action_date': '2018-04-23',
+      'last_action': 'Governor Signed',
+      'title': 'Agricultural Marketing Act Notices And Inclusion Of Millet'
+    },
+    '2': {
+      'relevance': 97,
+      'state': 'CO',
+      'bill_number': 'SJR003',
+      'bill_id': 1058674,
+      'change_hash': 'f7c5236b8a40fcf0588099d25cf5ef3b',
+      'url': 'https://legiscan.com/CO/bill/SJR003/2018',
+      'text_url': 'https://legiscan.com/CO/text/SJR003/2018',
+      'research_url': 'https://legiscan.com/CO/research/SJR003/2018',
+      'last_action_date': '2018-02-13',
+      'last_action': 'Governor Signed',
+      'title': 'Water Projects Eligibility Lists'
+    },
+    '3': {
+      'relevance': 86,
+      'state': 'CO',
+      'bill_number': 'HB1025',
+      'bill_id': 1053085,
+      'change_hash': '276706b908de6fc6d33dd66381cd021a',
+      'url': 'https://legiscan.com/CO/bill/HB1025/2018',
+      'text_url': 'https://legiscan.com/CO/text/HB1025/2018',
+      'research_url': 'https://legiscan.com/CO/research/HB1025/2018',
+      'last_action_date': '2018-04-23',
+      'last_action': 'Governor Signed',
+      'title': 'Relocate Title 12 Liquor Laws To Title 44'
+    },
+    'summary': {
+      'page': '1 of 1',
+      'range': '1 - 4',
+      'relevancy': '100% - 86%',
+      'count': 4,
+      'page_current': 1,
+      'page_total': 1,
+      'query': '(Zsugar:(pos=1))'
+    }
+  }
+}
