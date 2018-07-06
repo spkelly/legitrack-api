6170ecae8c03c980f90d27496d6fd25691025e70
spkelly <spkelly18@gmail.com>
2018-07-06 12:50:14 -0600

    removed fake bill endpoint

--- routes/bill.js
+++ routes/bill.js
@@ -3,6 +3,5 @@ const router = express.Router();
 const billService = require('../services/billServices');
 
 router.get('/:id', billService.getBill);
-router.get('/test', billService.getFakeBill);
 
 module.exports = router;
--- services/billServices.js
+++ services/billServices.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const fakeBill = require('../test/testBill');
+// const fakeBill = require('../test/testBill');
 
 require('dotenv').config();
 
@@ -13,10 +13,10 @@ async function getBill(req,res){
 }
 
 // sends a fake bill to the client 
-async function getFakeBill(req,res){
-  let fakeBill= await generateFakeBill();
-  res.json(fakeBill);
-}
+// async function getFakeBill(req,res){
+//   let fakeBill= await generateFakeBill();
+//   res.json(fakeBill);
+// }
 
 // fetches bill data from legiscan api
 function fetchBillData(id){
@@ -24,13 +24,13 @@ function fetchBillData(id){
 }
 
 // generates a fake bill for testing purposes
-function generateFakeBill(){
-  return new Promise((resolve,reject)=>{
-    setTimeout(()=>{
-      resolve(fakeBill);
-    }, 500);
-  });
-};
+// function generateFakeBill(){
+//   return new Promise((resolve,reject)=>{
+//     setTimeout(()=>{
+//       resolve(fakeBill);
+//     }, 500);
+//   });
+// };
 
 // generates a bill object from the raw bill data 
 function generateBill(billData){
@@ -49,7 +49,6 @@ function generateBill(billData){
 
 module.exports = {
   getBill,
-  getFakeBill
 };
 
 
