9730ea5b9d7b442bf900a43f4c77f64df08f4b89
spkelly <spkelly18@gmail.com>
2018-07-11 10:58:01 -0600

    modified votes to be more compatible with ChartJs

--- services/billServices.js
+++ services/billServices.js
@@ -32,8 +32,75 @@ function fetchBillData(id){
 //   });
 // };
 
+
+
+
+
+// "votes": [
+// {
+//   "roll_call_id": 751588,
+//   "date": "2018-05-09",
+//   "desc": "Senate: Consideration of Resolutions Resolution",
+//   "yea": 35,
+//   "nay": 0,
+//   "nv": 0,
+//   "absent": 0,
+//   "total": 35,
+//   "passed": 1,
+//   "chamber": "S",
+//   "chamber_id": 22,
+//   "url": "https://legiscan.com/CO/rollcall/HJR1022/id/751588",
+//   "state_link": "http://leg.colorado.gov/content/hjr18-1022vote19d54a"
+//   },
+//   {
+//   "roll_call_id": 751589,
+//   "date": "2018-05-09",
+//   "desc": "House: Third Reading Res",
+//   "yea": 64,
+//   "nay": 0,
+//   "nv": 0,
+//   "absent": 1,
+//   "total": 65,
+//   "passed": 1,
+//   "chamber": "H",
+//   "chamber_id": 21,
+//   "url": "https://legiscan.com/CO/rollcall/HJR1022/id/751589",
+//   "state_link": "http://leg.colorado.gov/content/hjr18-1022vote189402"
+//   }
+//   ],
+
+// used to make legiscan data more compatible with the ChartJs API
+function chartifyVotes(votes){
+  return votes.map(({desc, url, state_link, ...vote})=>{
+    return { 
+      label: vote.date,
+      data:[  
+        vote.yea,
+        vote.nay,
+        vote.nv, 
+        vote.absent
+        ],
+      desc,
+      url,
+      state_link
+    }
+  });
+}
+
+// const testData = {
+//  labels: ["Yay", "Nay", "NV", "Pass"],
+//    datasets:[{
+//      label:"number of votes",
+//      data:[52,65,0,0],
+//      backgroundColor: ['#777','#333']
+//   }
+// ]} 
+
 // generates a bill object from the raw bill data 
 function generateBill(billData){
+
+  console.log(chartifyVotes(billData.votes));
+
   return {
     'id'          :billData.bill_id,
     'title'       :billData.title,
